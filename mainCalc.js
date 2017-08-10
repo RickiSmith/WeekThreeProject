96ab31a7a90900f8dfb6ed116bcfdd0f258ab727
Ricki Smith <RickiS4@icloud.com>
2017-08-10 16:23:17 -0400

    Updated styling

--- mainCalc.js
+++ mainCalc.js
@@ -1,32 +1,18 @@
-// let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-// for (var i = 0; i < numbers.length; i++) {
-//   console.log(numbers[i]);
-// }
 
-var box = document.getElementById("display");
-
-function toScreen(x){
-  box.value += x;
-  if(x==="c"){
-    box.value = "";
-  }
-}
-
-function equal(x){
-  x=box.value;
-  x=eval(x);
-  box.value = x;
-}
-
-function power(x){
-  x=box.value;
-  x=eval(x*x);
-  box.value = x;
-}
-
-function backspace(){
-  var num = box.value;
-  var len = num.length - 1;
-  var newNum = num.substring(0,len);
-  box.value = newNum;
+let box = document.getElementById("display");
+let keys = document.querySelectorAll(".keys");
+for(var i = 0; i < keys.length; i++) {
+    keys[i].addEventListener("click", function () {
+        var btnVal = this.value;
+        if(btnVal == 'C') {
+            box.value = '';
+          } else if(btnVal == '=') {
+            var equation = box.value;
+            if(equation) {
+                box.value = eval(equation);
+            }
+        } else {
+            box.value += btnVal;
+        }
+    })
 }
